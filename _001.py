8c0a90392eeb18741a120877d3297e73264a97c1
kammitama5 <krystal.maughan@gmail.com>
2016-10-30 06:29:52 -0700

    going through re-cap of types in Pyth0n.

---
+++ _001.py
@@ -0,0 +1,21 @@
+import math
+print(math.sqrt(16)) # prints 4.0
+print("Algorithms and Data Structures");
+print(2+3*4) # prints 14
+print((2+3)*4) # prints 20
+print(2**10) # prints 1024
+print(6/3) # prints 2
+print(7//3) # prints 2
+print(7%3) # prints 1
+print(3/6) # prints 0
+print(3//6) # prints 0
+print(3%6) # prints 3
+print(2**100) # prints 1267650600228229401496703205376
+print(True) # prints True
+print(False) # prints False
+print(False or True) # prints True
+print(not (False or True)) # prints False
+print(True and False) # P
+print(5==10) # prints False
+print(10 > 5) # prints True
+print((5 >= 1) and (5 <= 10)) # prints True

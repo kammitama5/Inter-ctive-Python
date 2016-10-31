5c2a5c701010241357383f2cfd0a41d0de76c39f
kammitama5 <krystal.maughan@gmail.com>
2016-10-31 07:46:06 -0700

    in, len in sets

---
+++ _005.py
@@ -0,0 +1,5 @@
+mySet = {3, 6, "cat", 4.5, False}
+mySet # prints {3, 6, "cat", 4.5, False}
+len(mySet) # prints 5
+False in mySet # prints True -> is False an element in mySet
+"dog" in mySet # prints false

8b3365e4037d4fa92222cb0a01ac711ef174d6e9
kammitama5 <krystal.maughan@gmail.com>
2016-10-31 07:15:24 -0700

    string manip, lists

---
+++ _004.py
@@ -0,0 +1,21 @@
+range(10) # prints [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+list(range(10)) # prints [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]\
+
+range(5, 10) # prints [5, 6, 7, 8, 9]
+
+list(range(5, 10, 2)) # [5, 7, 9]
+
+list (range(10, 1, -1)) # [10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+myName = "David"
+
+myName.upper() # prints "DAVID"
+
+myName.center(10) #   David   ' -> returns a string centered in a field of size 10
+
+myName.find('v') # prints 2
+
+myName.split('v') # prints ['Da', 'id'] -> Splits a string into substrings at 'v'
+
+

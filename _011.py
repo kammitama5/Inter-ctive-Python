2d0afe9cf037374f84ccc3f6e4f3003b9aac1143
kammitama5 <krystal.maughan@gmail.com>
2016-10-31 11:14:59 -0700

    funny little square root programme

---
+++ _011.py
@@ -0,0 +1,21 @@
+import math
+
+def squareroot(n):
+	for n in range(1, 10):
+		print("square root is %.2f for value %d"%(n ** .5, n))
+	
+		
+	
+	
+print(squareroot(2)) # --> just a pass through
+
+# prints 
+# square root is 1.00 for value 1
+# square root is 1.41 for value 2
+# square root is 1.73 for value 3
+# square root is 2.00 for value 4
+# square root is 2.24 for value 5
+# square root is 2.45 for value 6
+# square root is 2.65 for value 7
+# square root is 2.83 for value 8
+# square root is 3.00 for value 9		

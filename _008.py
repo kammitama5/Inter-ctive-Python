2c61a0293eca03cde5f00190a5b941fd87f4d8a9
kammitama5 <krystal.maughan@gmail.com>
2016-10-31 10:18:01 -0700

    1st commit

---
+++ _008.py
@@ -0,0 +1,13 @@
+import math
+
+sqlist = []
+for x in range(1, 11):
+	sqlist.append(x * x)
+
+print(sqlist) # prints [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+sqlist = [ x * x for x in range(1,11)]
+print(sqlist) # prints [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+sqlist = [x * x for x in range(1,11) if x%2 != 0]
+print(sqlist) # prints [1, 9, 25, 49, 81]
